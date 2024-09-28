fe5f9cb3e17e4ce9ada6f28580d3958811a9eaf3
bhavyavemula0303 <bhavyavemula0303@gmail.com>
2024-09-28 18:06:25 +0530

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import './Footer.css';
+import "./Footer.css";
 import UserContext from "../context/UserContext";
 import { useLocation, useNavigate } from "react-router-dom";
 
@@ -15,9 +15,9 @@ const Footer = () => {
     <footer className="footer">
       <div className="footer-top">
         <div className="footer-logo">
-         <img src='../Logo.png' alt='logo'></img>
+          <img src="../Logo.png" alt="logo"></img>
         </div>
-        <div className="footer-links">
+        {/* <div className="footer-links">
           <div className="footer-column">
             <h3>Mobile app</h3>
             <ul>
@@ -42,18 +42,46 @@ const Footer = () => {
               <li>History</li>
             </ul>
           </div>
-        </div>
+        </div> */}
+        <div className="contact-div">
+            <div>{/* <label>contect info </label> */}</div>
+            <div>
+              <div className="info-main">
+                <div className="info">
+                  <label>contact</label>
+                </div>
+                {/* <div className="info">
+                  <label>Name </label>
+                  <label>divi gnanesh</label>
+                </div> */}
+                <div className="info">
+                  <label>Mobile </label>
+                  <label>9100828649</label>
+                </div>
+                <div className="info">
+                  <label>email </label>
+                  <label>divignanesh@gmail.com</label>
+                </div>
+              </div>
+            </div>
+          </div>
         {!userDetails ? (
-        <div className="footer-buttons">
-          <button className="login-button" onClick={() => openAuthModal("login")}>Login / Signup</button>
-        </div>) : <></>}
-
+          <div className="footer-buttons">
+            <button
+              className="login-button"
+              onClick={() => openAuthModal("login")}
+            >
+              Login / Signup
+            </button>
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
       <div className="footer-bottom">
         <div className="footer-copyright">
           <p>© Rewardsy, Inc. 2024. We love our users!</p>
         </div>
-        
       </div>
     </footer>
   );
